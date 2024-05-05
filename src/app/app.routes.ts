86663138095f60223f1fb6f5223a232c16774be7
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-05-05 13:04:36 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EjemplosAmaterialComponent } from './dashboard/ejemplos-amaterial/ejemplos-amaterial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "ejemplo-amaterial", component: EjemplosAmaterialComponent}
+];
