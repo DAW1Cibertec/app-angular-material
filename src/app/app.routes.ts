2ddd38b3c6177a77ed4cc6c0e573fc906b656e1c
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-05-11 20:45:48 -0500

    definición de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,27 @@
 import { Routes } from '@angular/router';
 import { EjemplosAmaterialComponent } from './dashboard/ejemplos-amaterial/ejemplos-amaterial.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { CursoListaComponent } from './dashboard/cursos/curso-lista/curso-lista.component';
+import { PostListaComponent } from './dashboard/posts/post-lista/post-lista.component';
+import { CursoDetalleComponent } from './dashboard/cursos/curso-detalle/curso-detalle.component';
+import { PostDetalleComponent } from './dashboard/posts/post-detalle/post-detalle.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path: "ejemplo-amaterial", component: EjemplosAmaterialComponent}
+    {path: "login", component: LoginComponent},
+    {
+        path: 'dashboard', component: DashboardComponent,
+        children: [
+            {path: 'home', component: HomeComponent},
+            {path: 'curso', component: CursoListaComponent},
+            {path: 'curso/:id', component: CursoDetalleComponent},
+            {path: 'ejemplos-amaterial', component: EjemplosAmaterialComponent},
+            {path: 'post', component: PostListaComponent},
+            {path: 'post/:id', component: PostDetalleComponent},
+        ]
+    },
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
 ];
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
