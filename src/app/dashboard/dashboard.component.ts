4c358e8911d5e9711e665434f0d4a70b20030df1
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-05-11 21:05:31 -0500

    menu de opciones con redirección

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../angular-material/material/material.module';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  constructor(private route: ActivatedRoute,
+    private router: Router
+  ){
+
+  }
+  
+  irPaginaHome(): void{
+    this.router.navigate(['home'], {relativeTo: this.route});
+  }
+  irPaginaEjemplos(): void{
+    this.router.navigate(['ejemplos-amaterial'], {relativeTo: this.route});
+  }
+  irPaginaCurso(): void{
+    this.router.navigate(['curso'], {relativeTo: this.route});
+  }
+  irPaginaPost(): void{
+    this.router.navigate(['post'], {relativeTo: this.route});
+  }
+  cerrarSesion(): void{
+    this.router.navigate(['login']);
+  }
 
 }
